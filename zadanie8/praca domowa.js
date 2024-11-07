38451ceee14bf564cc81bac492117a66436b3cd2
Malgorzata881 <m.michalczuk7@o2.pl>
2024-11-07 16:32:36 +0100

    commit

---
+++ zadanie8/praca domowa.js
@@ -0,0 +1,33 @@
+// zadanie 1
+// Pary klucz : wartość oraz typ danych wartości :
+// 1. ”name” : ”Josh” /string
+// 2. ”weight”: 175 /number
+// 3. ”age”: 30 /number
+// 4. ”eyecolor” : ”brown” /string
+// 5. ”isHappy” : true /boolean
+// 6. ”cars” : [”Chevy”, ”Honda”] /array
+// 7. ”favouriteBook” : {”title”: 8.38} /object
+// 8. ”title” : ”The Last Kingdom” /string
+// 9. ”author” : ”Bernard Cornwell” /string
+// 10. ”rating” : 8.38 /number
+
+// JSON jest zbudowany w oparciu o dwie struktury: zbiór par nazwa/wartość oraz uporządkowana lista wartości. 
+
+const json = '{"name":"Mango","age":3,"isHappy":true}'
+
+// zadanie 2
+// https://i.imgur.com/o0Hz0gX.png
+// konwersja wykonała się poprawnie
+
+// zadanie 3
+// Próba użycia 'parse' z błędnym kodem JSON :
+// https://i.imgur.com/lu6Od6Z.png
+// Dostaliśmy błąd przy użyciu niepoprawnego formatu JSON
+
+// Próba z niepoprawnymi danymi z użyciem funkcji try ... catch:
+// https://i.imgur.com/mtz3FmH.png
+// Mimo błędu skrypt działa poprawnie
+
+
+
+
